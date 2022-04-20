38c6ae77367571aa764a1914300777e97f7b6125
psalessi-intel.com <psalessi-intel@noreply.git>
2022-04-20 13:35:35 -0700

    Update nosqli challenge

--- breakableflask/vulnerable_web_app/nosqli/nosqli.py
+++ breakableflask/vulnerable_web_app/nosqli/nosqli.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, flash, render_template, redirect
+from flask import Blueprint, flash, render_template
 from flask import current_app as app
 from flask_pymongo import PyMongo
+import json
 
 from .login_form import LoginForm
 from .config import *
@@ -19,19 +20,31 @@ def nosqli():
 
     form = LoginForm()
 
-    if form.validate_on_submit():
+    if form.validate_on_submit():      
         username = form.username.data
         password = form.password.data
 
-        # TODO: Make vulnerable
+        # The form inputs will come in as strings which protects against NoSQL injection. 
+        # In order to make this form vulnerable, we need to attempt to load the inputs as JSON
+        # But if an input is not JSON, the form still needs to work
+        try:
+            username = json.loads(username)
+        except ValueError as e:
+            pass
+        try:
+            password = json.loads(password)
+        except ValueError as e:
+            pass
+
         user_count = mongo.db.users.count_documents({
             "username": username,
             "password": password
         })
 
         if user_count > 0:
-            return render_template('flag.html')
+            flash(f"{FLAG}",)
+            return render_template("nosqli.html", form=form)
         else:
-            flash('Login failed')
+            flash("Login failed", "error")
 
-    return render_template('nosqli.html', form=form)
+    return render_template("nosqli.html", form=form)
